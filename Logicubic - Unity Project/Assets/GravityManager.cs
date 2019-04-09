56b867edb1eb14dfeabcaea62a2b2ad5090df52d
RaihanAk <raihanakbar1999@gmail.com>
2019-04-09 19:29:43 +0700

    init commit

---
+++ Logicubic - Unity Project/Assets/GravityManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityManager : MonoBehaviour {
+
+    public string gravityStatus;
+
+    void Start(){
+        gravityStatus = "down";
+    }
+
+    void OnCollisionEnter(Collision collisionInfo) {
+        
+        // Check what that object collides with 
+        // Debug.Log(collisionInfo.collider.name);
+
+        // Gravity to the LEFT of za warudo
+        if (collisionInfo.collider.tag == "plane_left") {
+            //Debug.Log("To the LEFT!");
+            gravityStatus = "left";
+
+            Physics.gravity = new Vector3(0, 0, 9.81f);
+        }
+
+        // Gravity to the RIGHT of za warudo
+        if (collisionInfo.collider.tag == "plane_right") {
+            //Debug.Log("To the RIGHT");
+            gravityStatus = "right";
+
+            Physics.gravity = new Vector3(0, 0, -9.81f);
+        }
+
+        // Gravity to UPSIDE-DOWN of the world
+        if (collisionInfo.collider.tag == "plane_up") {
+            //Debug.Log("UPSIDE-DOWN!");
+            gravityStatus = "up";
+
+            Physics.gravity = new Vector3(0, -9.81f, 0);
+        }
+    }
+
+    public string getGravityStatus(){
+        return this.gravityStatus;
+    }
+}
---
+++ Logicubic - Unity Project/Assets/cube.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+
+    public float moveSpeed;
+    private GravityManager gm;
+    public string gravityDirection;
+
+	// Use this for initialization
+	void Start () {
+        //GameObject gravityManager = new GameObject("GravityManager");
+
+        gm = GameObject.FindObjectOfType<GravityManager>();
+        
+        gravityDirection = "down";
+
+        moveSpeed = 7f;
+        print("Yo im a cube. Called from the start");
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        /* Manage to move under certain gravity */ 
+        if (gravityDirection != gm.getGravityStatus()){
+            gravityDirection = gm.getGravityStatus();
+            //changeBoxFace(gravityDirection);
+            Debug.Log("Gravity direction: " +gravityDirection);
+        }
+
+        /* If under normal gravity -- "down" */
+        if (gravityDirection == "down"){
+            transform.Rotate(0.0f, 2.0f * Input.GetAxis("Mouse X"), 0.0f);
+            transform.Translate(
+                moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 
+                0.0f, 
+                moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        }
+        /* If under normal gravity -- "left" */
+        if (gravityDirection == "left"){
+            transform.Rotate(0.0f, 2.0f * Input.GetAxis("Mouse Y"), 0.0f);
+            transform.Translate(
+                moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime,
+                moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 
+                0.0f);
+        }
+
+        // Check gravity status -- gm.getGravityStatus();
+	}
+
+    void changeBoxFace (string direction){
+        if (direction == "left"){
+            transform.Rotate(-90.0f, 0.0f, 0.0f);
+        }
+    }
+}
