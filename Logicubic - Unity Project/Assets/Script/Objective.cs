a6134a29455e6093c2da7e8f7311e844456b32ef
RaihanAk <raihandoank@ymail.com>
2019-05-07 17:38:00 +0700

    Test logic kalo nyampe finish ada suara2an

---
+++ Logicubic - Unity Project/Assets/Script/Objective.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Objective : MonoBehaviour {
+
+	AudioSource audioData;
+
+	// Use this for initialization
+	void Start () {
+		audioData = GetComponent<AudioSource>();	
+	}
+
+	void OnCollisionEnter (Collision collisionInfo){
+		if (collisionInfo.collider.tag == "Player"){
+			audioData.Play(0);
+			Debug.Log("Started Playing");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
