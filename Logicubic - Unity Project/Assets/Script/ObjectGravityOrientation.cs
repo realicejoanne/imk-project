f3fca7ee9ddf1c5c9a9bb197be25720fa368bd02
RaihanAk <raihandoank@ymail.com>
2019-05-13 13:36:06 +0700

    test imk particle

---
+++ Logicubic - Unity Project/Assets/Script/ObjectGravityOrientation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGravityOrientation : MonoBehaviour {
+
+	Rigidbody rigidbody;
+	GravityManager gm;
+	public string gravityOrientation = "down";
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+		gm = GameObject.FindObjectOfType<GravityManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gravityOrientation == gm.getGravityStatus()){
+			rigidbody.useGravity = true;
+		}
+		else
+			rigidbody.useGravity = false;
+	}
+}
