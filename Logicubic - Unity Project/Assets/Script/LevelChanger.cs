4c915b877339717a8995a339fdaaec58eed70041
RaihanAk <raihandoank@ymail.com>
2019-05-22 14:49:19 +0700

    Scene-changing in a level

---
+++ Logicubic - Unity Project/Assets/Script/LevelChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelChanger : MonoBehaviour {
+
+	public string targetLevel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter (Collision collisionInfo){
+		if (collisionInfo.collider.tag == "Player"){
+			SceneManager.LoadScene(targetLevel);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
