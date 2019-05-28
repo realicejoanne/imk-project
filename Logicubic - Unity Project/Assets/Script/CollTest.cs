f7bdbd7de8de85d76582654e5abbae8fac96ddec
RaihanAk <raihandoank@ymail.com>
2019-05-28 21:34:11 +0700

    Only change gravity when FACEing wall

---
+++ Logicubic - Unity Project/Assets/Script/CollTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter(Collision collisionInfo) {
+		Debug.Log("COLLIDE");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
