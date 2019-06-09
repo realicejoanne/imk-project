cf54e8aa229dbd9e7fb11690d5b53156707dd7e2
RaihanAk <raihandoank@ymail.com>
2019-06-09 23:31:27 +0700

    user will ALWAYS plays tutorial at least once until completed

--- Logicubic - Unity Project/Assets/Script/ButtonSceneChanger.cs
+++ Logicubic - Unity Project/Assets/Script/ButtonSceneChanger.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 
 public class ButtonSceneChanger : MonoBehaviour {
 
+	int isCompleteTutorial;
+
 	// Use this for initialization
 	public void SceneChange (string sceneName){
 		SceneManager.LoadScene(sceneName);
 	}
 
+	public void StartGameScene (){
+		// is the player have completed the tutorial?
+		isCompleteTutorial = PlayerPrefs.GetInt("isCompleteTutorial", 0);
+		Debug.Log(isCompleteTutorial);
+
+		// if not yet, go to tutorial
+		if(isCompleteTutorial == 0)
+			SceneManager.LoadScene("Level 0");
+		// if yes, go to level picker
+		else
+			SceneManager.LoadScene("Level Picker");
+	}
 }
---
+++ Logicubic - Unity Project/Assets/Script/TutorialCompletion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCompletion : MonoBehaviour {
+
+	void OnCollisionEnter (Collision collisionInfo){
+		if (collisionInfo.collider.tag == "Player"){
+			PlayerPrefs.SetInt("isCompleteTutorial", 1);
+			Debug.Log(PlayerPrefs.GetInt("isCompleteTutorial", 0));
+		}
+	}
+}
