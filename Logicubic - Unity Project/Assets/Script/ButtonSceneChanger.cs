54b096b771d5b515b16dd8891ae6772eda82ac36
RaihanAk <raihandoank@ymail.com>
2019-06-09 23:10:51 +0700

    button works now

---
+++ Logicubic - Unity Project/Assets/Script/ButtonSceneChanger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonSceneChanger : MonoBehaviour {
+
+	// Use this for initialization
+	public void SceneChange (string sceneName){
+		SceneManager.LoadScene(sceneName);
+	}
+
+}
