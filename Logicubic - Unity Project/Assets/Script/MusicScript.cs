5f8d5e0d29ae9fb6f54ec388d7ef63a9ac218224
RaihanAk <raihandoank@ymail.com>
2019-06-13 15:03:27 +0700

    Music for main menu

---
+++ Logicubic - Unity Project/Assets/Script/MusicScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicScript : MonoBehaviour {
+	static bool AudioBegin = false; 
+	AudioSource audioData;
+	Scene sceneCurrent;
+
+	// Use this for initialization
+	void Start () {
+		audioData = GetComponent<AudioSource>();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (!AudioBegin) {
+			audioData.Play (0);
+			DontDestroyOnLoad (gameObject);
+			AudioBegin = true;
+		}
+
+		sceneCurrent = SceneManager.GetActiveScene();
+		if (sceneCurrent.name == "Level 0" || sceneCurrent.name == "Level Picker"){
+			Debug.Log(sceneCurrent.name);
+		}
+	}
+}
